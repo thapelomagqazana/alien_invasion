f64b7f327ddf361c4d38d56f2d796f26430acab6
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-06-11 17:33:41 +0200

    Adjusted the Ship's Speed, Linited the Ship's Range, Refactored _check_events()

--- settings.py
+++ settings.py
@@ -7,3 +7,6 @@ class Settings:
         self.screen_height = 600
         # Set the background colour.
         self.bg_colour = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 1.5
